70c85f4d6d535543b64d20ad68b556fc36f69e60
Calder Niven <calderjeniven@outlook.com>
2020-09-02 18:53:44 +0100

    added character select and active character functionality

--- src/js/components/CharacterSelect.js
+++ src/js/components/CharacterSelect.js
@@ -1,11 +1,28 @@
 "use strict";
 
 import { BaseComponent } from "./BaseComponent.js";
+import { App } from "../utils/App.js";
+import { $ } from "../utils/helpers.js";
 
 export class CharacterSelect extends BaseComponent {
 
     render() {
-        this.element.innerHTML = "this is a Character-Select element";
+        let player = window.app.player
+        let characterList = player.characters.roster;
+        let characters = ""
+        for (let index = 0; index < characterList.length; index++) {
+            const character = characterList[index];
+            characters += `<option value=${index}>${character.name}</option>`
+        }
+        
+        let element = `<select>${characters}</select>`;
+        this.element.innerHTML = element;
+
+        let dropDown = $("select", this.element);
+        dropDown.onchange = function(selection) {
+            window.app.player.characters.active = Number(selection.target.value);
+        }
+
     }
 
 }
