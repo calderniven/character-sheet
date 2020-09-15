3e3c30a8e421a10da24092bf047a66e692642d69
Calder Niven <calderjeniven@outlook.com>
2020-09-15 18:26:51 +0100

    added save functionality for stat changes

--- src/js/components/CharacterStat.js
+++ src/js/components/CharacterStat.js
@@ -2,12 +2,23 @@
 
 import { BaseComponent } from "./BaseComponent.js";
 import { Character } from "../models/Character.js";
+import { $ } from "../utils/helpers.js";
+import { Player } from "../models/Player.js";
 
 export class CharacterStat extends BaseComponent {
     render() {
         let statName = this.attributes.getNamedItem("data-bind").value;
         let character = Character.active();
         let statValue = character.attributes[statName];
-        this.element.innerHTML = `<input type="number" value=${statValue}>`;
+        this.element.innerHTML = `<label>${statName}</label> <input data-bind="${statName}" type="number" value=${statValue}>`;
+        
+        let stat = $("input", this.element);
+        stat.onchange = function(event) {
+            let newValue = event.target.valueAsNumber;
+            let statName = event.target.attributes.getNamedItem("data-bind").value;
+
+            Character.active().attributes[statName] = newValue;
+            Player.active().save();
+        }
     }
 }
