a8d013aa5a418d2de55f9e8c963e04ff65c2be64
Calder Niven <calderjeniven@outlook.com>
2020-10-02 17:56:42 +0100

    added plus and minus buttons for stats

--- src/js/components/CharacterStat.js
+++ src/js/components/CharacterStat.js
@@ -10,16 +10,34 @@ export class CharacterStat extends BaseComponent {
         let statName = this.attributes.getNamedItem("data-bind").value;
         let character = Character.active();
         let statValue = character.attributes[statName];
-        this.element.innerHTML = `<label>${statName}</label> <input data-bind="${statName}" type="number" value=${statValue}>`;
+        this.element.innerHTML =
+            `<label>${statName}</label>
+        <button class="plus-button">+</button>
+        <input data-bind="${statName}" type="number" value=${statValue}>
+        <button class="minus-button">-</button>`;
         this.element.className = "character-stat";
-        
+
         let stat = $("input", this.element);
-        stat.onchange = function(event) {
+        stat.onchange = function (event) {
             let newValue = event.target.valueAsNumber;
             let statName = event.target.attributes.getNamedItem("data-bind").value;
 
             Character.active().attributes[statName] = newValue;
             Player.active().save();
         }
+
+        $("button.plus-button", this.element).onclick = function (event) {
+            CharacterStat.onButtonClick(event, +1);
+        }
+
+        $("button.minus-button", this.element).onclick = function (event) {
+            CharacterStat.onButtonClick(event, -1);
+        }
+    }
+
+    static onButtonClick(event, number) {
+        let input = $("input", event.target.parentNode);
+        input.value = Number(input.value) + number;
+        input.onchange({ target: input });
     }
 }
